1ebb44394b8b1cd96574611517d4e814f8a50658
Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>
2020-06-25 14:30:03 +0200

    arch: arm: cortex_m: Add wrap_func.h
    Provide a WRAP_FUNC() macro which helps in creating a function
    that "wraps" another in a preface and postface function call.
    
    	int foo(char *arg); // Implemented somewhere else.
    	int __attribute__((naked)) foo_wrapped(char *arg)
    		{WRAP_FUNC(bar, foo, baz)}
    
    is equivalent to
    
    	int foo(char *arg); // Implemented somewhere else.
    	int foo_wrapped(char *arg)
    	{
    		bar();
    		int res = foo(arg);
    		baz();
    		return res;
    	}
    
    This commit also adds tests for the new macros.
    
    Signed-off-by: Øyvind Rønningstad <oyvind.ronningstad@nordicsemi.no>

---
+++ arch/arm/include/aarch32/cortex_m/wrap_func.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2020 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief
+ */
+
+#ifndef ZEPHYR_ARCH_ARM_INCLUDE_AARCH32_CORTEX_M_WRAP_FUNC_H_
+#define ZEPHYR_ARCH_ARM_INCLUDE_AARCH32_CORTEX_M_WRAP_FUNC_H_
+
+#ifdef _ASMLANGUAGE
+
+/* nothing */
+
+#else
+
+/**
+ * @brief Macro for "sandwiching" a function call (@p name) in two other calls
+ *
+ * This macro should be called via @ref WRAP_FUNC or @ref WRAP_FUNC_STACK_ARGS.
+ *
+ * This macro creates the function body of an "outer" function which behaves
+ * exactly like the wrapped function (@p name), except that the preface function
+ * is called before, and the postface function afterwards.
+ *
+ * @param preface   The function to call first. Must have no parameters and no
+ *                  return value.
+ * @param name      The main function, i.e. the function to wrap. This function
+ *                  will receive the arguments, and its return value will be
+ *                  returned.
+ * @param postface  The function to call last. Must have no parameters and no
+ *                  return value.
+ * @param store_lr  The assembly instruction for storing away the LR value
+ *                  before the functions are called.
+ * @param load_lr   The assembly instruction for restoring the LR value after
+ *                  the functions have been called.
+ */
+#define WRAP_FUNC_RAW(preface, name, postface, store_lr, load_lr) \
+	do { \
+		__asm(".global "#preface"; .type "#preface", %function"); \
+		__asm(".global "#name"; .type "#name", %function"); \
+		__asm(".global "#postface"; .type "#postface", %function"); \
+		\
+		__asm(store_lr); \
+		__asm("push {r0-r3}"); \
+		__asm("bl "#preface); \
+		__asm("pop {r0-r3}"); \
+		__asm("bl "#name); \
+		__asm("push {r0-r3}"); \
+		__asm("bl "#postface); \
+		__asm("pop {r0-r3}"); \
+		__asm(load_lr); \
+	} while (0)
+
+/**
+ * @brief Macro for "sandwiching" a function call (@p name) in two other calls
+ *
+ * This is for functions which do not pass arguments or return values via the
+ * stack. I.e. the arguments and return values must each fit within 4 words,
+ * after accounting for alignment.
+ *
+ * If nothing is passed on the stack, the stack can be used to store LR without
+ * messing with offsets.
+ *
+ *	int foo(char *arg); // Implemented elsewhere.
+ *	int __attribute__((naked)) foo_wrapped(char *arg)
+ *		{WRAP_FUNC(bar, foo, baz)}
+ *
+ * is equivalent to
+ *
+ *	int foo(char *arg); // Implemented elsewhere.
+ *	int foo_wrapped(char *arg)
+ *	{
+ *		bar();
+ *		int res = foo(arg);
+ *		baz();
+ *		return res;
+ *	}
+ *
+ * @note __attribute__((naked)) is not mandatory, but without it, GCC gives a
+ *       warning for functions with a return value. It also saves a bit of space
+ *       since it removes a little code that is not necessary.
+ *
+ * See @ref WRAP_FUNC_RAW for more information.
+ */
+#define WRAP_FUNC(preface, name, postface) \
+	WRAP_FUNC_RAW(preface, name, postface, "push {lr}", "pop {pc}")
+
+/**
+ * @brief Macro for "sandwiching" a function call (@p name) in two other calls
+ *
+ * This is for functions which pass arguments or return values via the stack.
+ * I.e. the arguments or return values exceed 4 words, after accounting for
+ * alignment.
+ *
+ * This means LR cannot be stored on the stack, so it must be stored in an extra
+ * argument called lr_bak, see example below.
+ *
+ *	int foo(char *arg1, char *arg2, int arg3, uint64 arg4);
+ *	int __attribute__((naked)) foo_wrapped(char *arg1, char *arg2, int arg3,
+ *						uint64 arg4, uint32_t lr_bak)
+ *		{WRAP_FUNC_STACK_ARGS(bar, foo, baz)}
+ *
+ * is equivalent to
+ *
+ *	int foo(char *arg1, char *arg2, int arg3, uint64 arg4);
+ *	int foo_wrapped(char *arg1, char *arg2, int arg3, uint64 arg4,
+ *			uint32_t lr_bak)
+ *	{
+ *		bar();
+ *		int res = foo(arg1, arg2, arg3, arg4);
+ *		baz();
+ *		return res;
+ *	}
+ *
+ * @note __attribute__((naked)) is not mandatory, but without it, GCC gives a
+ *       warning for functions with a return value. It also saves a bit of space
+ *       since it removes a little code that is not necessary.
+ *
+ * See @ref WRAP_FUNC_RAW for more information.
+ */
+#define WRAP_FUNC_STACK_ARGS(preface, name, postface) \
+	WRAP_FUNC_RAW(preface, name, postface, "str lr, %0" :: "m" (lr_bak), \
+			"ldr pc, %0" :: "m" (lr_bak))
+
+#endif /* _ASMLANGUAGE */
+#endif /* ZEPHYR_ARCH_ARM_INCLUDE_AARCH32_CORTEX_M_WRAP_FUNC_H_ */
---
+++ tests/arch/arm/arm_wrap_func/src/main.c
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+#include <../arch/arm/include/aarch32/cortex_m/wrap_func.h>
+
+static bool preface_called;
+static bool postface_called;
+static bool foo1_called;
+static uint32_t foo1_retval;
+static uint32_t foo1_arg1;
+static uint32_t foo1_arg2;
+static uint32_t foo1_arg3;
+static uint32_t foo1_arg4;
+static bool foo2_called;
+static uint64_t foo2_retval;
+static uint32_t foo2_arg1;
+static uint32_t foo2_arg2;
+static uint32_t foo2_arg3;
+static uint64_t foo2_arg4;
+
+void reset_mocks(void)
+{
+	preface_called = 0;
+	postface_called = 0;
+	foo1_called = 0;
+	foo1_retval = 0;
+	foo1_arg1 = 0;
+	foo1_arg2 = 0;
+	foo1_arg3 = 0;
+	foo1_arg4 = 0;
+	foo2_called = 0;
+	foo2_retval = 0;
+	foo2_arg1 = 0;
+	foo2_arg2 = 0;
+	foo2_arg3 = 0;
+	foo2_arg4 = 0;
+}
+
+void preface(void)
+{
+	zassert_false(preface_called, "%s already called", __func__);
+	preface_called = true;
+}
+void postface(void)
+{
+	zassert_false(postface_called, "%s already called", __func__);
+	postface_called = true;
+}
+uint32_t foo1(uint32_t arg1, uint32_t arg2, uint32_t arg3, uint32_t arg4)
+{
+	zassert_false(foo1_called, "%s already called", __func__);
+	zassert_equal(arg1, foo1_arg1, "1");
+	zassert_equal(arg2, foo1_arg2, "2");
+	zassert_equal(arg3, foo1_arg3, "3");
+	zassert_equal(arg4, foo1_arg4, "4");
+	foo1_called = true;
+	return foo1_retval;
+}
+uint64_t foo2(uint32_t arg1, uint32_t arg2, uint32_t arg3, uint64_t arg4)
+{
+	zassert_false(foo2_called, "%s already called", __func__);
+	zassert_equal(arg1, foo2_arg1, "5");
+	zassert_equal(arg2, foo2_arg2, "6");
+	zassert_equal(arg3, foo2_arg3, "7");
+	zassert_equal(arg4, foo2_arg4, "8");
+	foo2_called = true;
+	return foo2_retval;
+}
+
+uint32_t __attribute__((naked)) wrap_foo1(uint32_t arg1, uint32_t arg2,
+				uint32_t arg3, uint32_t arg4)
+	{WRAP_FUNC(preface, foo1, postface); }
+
+uint64_t __attribute__((naked)) wrap_foo2(uint32_t arg1, uint32_t arg2,
+				uint32_t arg3, uint64_t arg4, uint32_t lr_bak)
+	{WRAP_FUNC_STACK_ARGS(preface, foo2, postface); }
+
+void test_arm_wrap_func(void)
+{
+	reset_mocks();
+	foo1_retval = 0x01234567;
+	foo1_arg1 = 0x12345678;
+	foo1_arg2 = 0x23456789;
+	foo1_arg3 = 0x3456789a;
+	foo1_arg4 = 0x456789ab;
+
+	uint32_t msp = __get_MSP();
+
+	zassert_equal(foo1_retval,
+		wrap_foo1(foo1_arg1, foo1_arg2, foo1_arg3, foo1_arg4), "9");
+
+	zassert_equal(msp, __get_MSP(), NULL);
+
+	zassert_true(preface_called, NULL);
+	zassert_true(foo1_called, NULL);
+	zassert_true(postface_called, NULL);
+
+	reset_mocks();
+	foo2_retval = 0x0123456789UL;
+	foo2_arg1 = 0x12345679;
+	foo2_arg2 = 0x2345678a;
+	foo2_arg3 = 0x3456789b;
+	foo2_arg4 = 0x456789abc;
+
+	uint64_t ret2 = wrap_foo2(foo2_arg1, foo2_arg2, foo2_arg3, foo2_arg4,
+				0);
+	zassert_equal(foo2_retval, ret2,
+		"wrong retval. Was 0x%"PRIx64", expected 0x%"PRIx64, ret2,
+		foo2_retval);
+
+	zassert_equal(msp, __get_MSP(), NULL);
+
+	zassert_true(preface_called, NULL);
+	zassert_true(foo2_called, NULL);
+	zassert_true(postface_called, NULL);
+}
+
+void test_main(void)
+{
+	ztest_test_suite(arm_wrap_func,
+		ztest_unit_test(test_arm_wrap_func));
+	ztest_run_test_suite(arm_wrap_func);
+}
